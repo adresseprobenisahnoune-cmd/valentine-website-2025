709a2abc5377581149a6a74ab4e01dc75c8e6592
adresseprobenisahnoune-cmd <adresseprobenisahnoune@gmail.com>
2026-02-03 06:32:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dywuaxt3j/video/upload/v1770096683/Bruno_Mars_-_Just_The_Way_You_Are_Lyrics_xgesuj.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
